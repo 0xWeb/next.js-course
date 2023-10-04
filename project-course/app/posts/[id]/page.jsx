6b73a1c67a1dc4fec69947ff12bb87e7d2f4475d
0xRKX <alexberarg95@gmail.com>
2023-10-04 18:12:07 +0200

    Params

---
+++ project-course/app/posts/[id]/page.jsx
@@ -0,0 +1,32 @@
+import PostPages from '../page'
+
+
+async function loadPost(id) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    const data = await res.json()
+    return data
+}
+
+async function Page({ params }) {
+
+    const post = await loadPost(params.id)
+
+    return (
+        <div>
+            <h1>
+                {post.id} {post.title}
+            </h1>
+            <p>
+                {post.body}
+            </p>
+            <hr />
+            <hr />
+
+            <h2>Other Posts</h2>
+
+            <PostPages />
+        </div>
+    )
+}
+
+export default Page
